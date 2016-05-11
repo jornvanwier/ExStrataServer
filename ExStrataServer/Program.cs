0adc44a65293a30319896e1d1aa775848a60d67a
Jorn van Wier <jornvanwier@gmail.com>
2016-05-11 14:44:44 +0200

    Initialize 9292 in program

--- ExStrataServer/Program.cs
+++ ExStrataServer/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ExStrataServer.ColorPattern;
+using ExStrataServer.APIs;
 
 namespace ExStrataServer
 {
@@ -12,6 +13,8 @@ namespace ExStrataServer
         static void Main(string[] args)
         {
             Log.Add("Piet", "Sok");
+
+            Watch9292 _9292 = new Watch9292(1000 * 15, "NHL Stenden Hogeschool");
         }
     }
 }
