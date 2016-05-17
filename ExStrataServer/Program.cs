943578ea8cb007ff428917e9c398ffae8b44c09f
Ruurd Bijlsma <ruurdbijlsma@gmail.com>
2016-05-17 13:18:47 +0200

    Preperations

--- ExStrataServer/Program.cs
+++ ExStrataServer/Program.cs
@@ -25,7 +25,22 @@ namespace ExStrataServer
                 new WatchTest(1000 * 60 * 2),
                 new Watch9292(1000 * 60, "NHL Stenden Hogeschool"));
 
-            manager.StartAll();
+            //manager.StartAll();
+
+            Pattern animationPattern = Pattern.Animate(new Pattern.GradientFrame[]{
+                new Pattern.GradientFrame(0, Frame.Gradient(new Frame.GradientColour[]
+                {
+                    new Frame.GradientColour(0, Colour.Red),
+                    new Frame.GradientColour(100, Colour.Blue)
+                })),
+                new Pattern.GradientFrame(100, Frame.Gradient(new Frame.GradientColour[]
+                {
+                    new Frame.GradientColour(0, Colour.Green),
+                    new Frame.GradientColour(100, Colour.Orange)
+                }))
+            }, "Animation", 200, 14);
+
+            ExStrataAPI.PlayPattern(animationPattern);
 
             Console.ReadKey();
 
