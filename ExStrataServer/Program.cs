d7d8266e3ef429700c3f46e655f729d2583a9cca
Ruurd Bijlsma <ruurdbijlsma@gmail.com>
2016-05-18 14:54:44 +0200

    weather api fix

--- ExStrataServer/Program.cs
+++ ExStrataServer/Program.cs
@@ -23,24 +23,12 @@ namespace ExStrataServer
 
             APIManager manager = new APIManager(
                 new WatchTest(1000 * 60 * 2),
-                new Watch9292(1000 * 60, "NHL Stenden Hogeschool"));
+                new Watch9292(1000 * 60, "NHL Stenden Hogeschool"),
+                new WatchWeather(1000 * 60, "NL", "Leeuwarden")
+                );
 
-            //manager.StartAll();
-
-            Pattern animationPattern = Pattern.Animate(new Pattern.GradientFrame[]{
-                new Pattern.GradientFrame(0, Frame.Gradient(new Frame.GradientColour[]
-                {
-                    new Frame.GradientColour(0, Colour.Red),
-                    new Frame.GradientColour(100, Colour.Blue)
-                })),
-                new Pattern.GradientFrame(100, Frame.Gradient(new Frame.GradientColour[]
-                {
-                    new Frame.GradientColour(0, Colour.Green),
-                    new Frame.GradientColour(100, Colour.Orange)
-                }))
-            }, "Animation", 200, 14);
-
-            ExStrataAPI.PlayPattern(animationPattern);
+            manager.StartAll();
+            
 
             Console.ReadKey();
 
